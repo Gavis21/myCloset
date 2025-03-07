b6315ed5d25fc5132afd81348e98fa048341b0c1
Guy <gavis45627@gmail.com>
2025-03-07 12:11:49 +0200

    Create login

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import UserSchema from "../models/user";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 
 const register = async (request: Request, response: Response) => {
@@ -43,6 +44,56 @@ const register = async (request: Request, response: Response) => {
     }
 }
 
+
+const login = async (request: Request, response: Response) => {
+    const email = request.body.email;
+    const password = request.body.password;
+
+    console.log(`${email} is trying to login`)
+
+    if (!email || !password){
+        console.error(`Login failed because of missing parameters`);
+        return response.status(400).send("Missing parameters");
+    }
+
+    console.log(`Fetching user with email ${email}`)
+    const user = await UserSchema.findOne({'email': email});
+    if (user == null) {
+        console.error(`User for ${email} doesn't exists`);
+        return response.status(404).send("User for that email doesn't exists");
+    } 
+
+    const isPasswdsMatches = await bcrypt.compare(password, user.password);
+    if (!isPasswdsMatches){
+        console.error(`Passwords for ${email} doesn't match - login failed`);
+        return response.status(401).send("Wrong email or password");
+    }
+
+    const accessToken = jwt.sign({_id: user._id}, process.env.JWT_SECRET as string, { expiresIn: process.env.JWT_EXPIRATION });
+    const refreshToken = jwt.sign({ _id: user._id }, process.env.JWT_REFRESH_SECRET as string);
+    if (!user.refreshTokens) {
+        user.refreshTokens = [refreshToken];
+    } else {
+        user.refreshTokens.push(refreshToken);
+    }
+    
+    try {
+        console.log(`Login completed for ${user._id}`);
+        await user.save();
+
+        return response.status(200).send({
+            'id': user._id,
+            'username': user.username,
+            'accessToken': accessToken,
+            'refreshToken': refreshToken
+        });
+    } catch (err) {
+        console.error(`Error login for - ${user._id}, \n ${err}`);
+        response.status(500).send(`Error login for - ${user._id}, \n ${err}`);
+    }
+}
+
 export default {
     register,
+    login
 }
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,8 +1,10 @@
-import express, { response } from "express";
+import express from "express";
 import authController from "../controllers/auth.controller";
 const router = express.Router();
 
 
 router.post("/register", authController.register);
+router.post("/login", authController.login);
+
 
 export default router;
