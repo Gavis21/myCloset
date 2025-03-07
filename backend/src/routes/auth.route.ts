111e1fc69bb9dd95cb2bc03456b0015ba4bc3969
Guy <gavis45627@gmail.com>
2025-03-07 12:14:45 +0200

    Create refresh token

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -6,6 +6,8 @@ const router = express.Router();
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
+router.get("/refresh", authController.refresh);
+
 
 
 export default router;
