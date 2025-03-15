ea0e714e3296eefe46f1ffa969a2307c96413cd2
Liron Shriki <liron757@gmail.com>
2025-03-15 21:15:39 +0200

    added basic auth implementaion

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { CssBaseline } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import AuthContext from "./auth/AuthContext.tsx";
+import { useEffect, useState } from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [currentUser, setCurrentUser] = useState(
+    localStorage.getItem("accessToken") || {}
+  );
+
+  useEffect(() => {
+    console.log("Updated default token");
+    // apiClient.defaults.headers.common = {'authorization': `bearer ${localStorage.getItem('accessToken')}`};
+  }, [currentUser]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <GoogleOAuthProvider clientId="179334466716-uvk404sbcsdvf7ptcjf3n1rqfi4sha95.apps.googleusercontent.com">
+        <AuthContext.Provider
+          value={{ user: currentUser, setUser: setCurrentUser }}
+        ></AuthContext.Provider>
 
-export default App
+        <CssBaseline />
+
+        <Outlet />
+      </GoogleOAuthProvider>
+    </>
+  );
+}
