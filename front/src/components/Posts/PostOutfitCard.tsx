a6c0e3b121c873ade70688a21c864e852d082824
Liron Shriki <liron757@gmail.com>
2025-03-18 19:17:47 +0200

    added post outfit card by mock

---
+++ front/src/components/Posts/PostOutfitCard.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Box, IconButton } from "@mui/material";
+import theme from "../../theme";
+import { useNavigate } from "react-router-dom";
+import { IPost } from "../../services/posts-service";
+import EditIcon from "@mui/icons-material/Edit";
+
+const OutfitCard = ({ post }: { post: IPost }) => {
+  const navigate = useNavigate();
+  const routeChange = (path: string) => navigate(path);
+  const routeOutfitPage = (_id: string) =>
+    routeChange(`/outfitPage?postId=${_id}`);
+
+  return (
+    <Card
+      sx={{ display: "flex" }}
+      style={{
+        width: "100%",
+        borderColor: theme.palette.primary.main,
+        borderWidth: "3px",
+        borderStyle: "solid",
+      }}
+    >
+      <CardMedia
+        component="img"
+        alt="Image"
+        sx={{ width: 151 }}
+        image={post.imageUrl}
+        style={{ padding: "16px" }}
+      />
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Box sx={{ display: "flex", flexDirection: "row" }}>
+            <Typography component="div" variant="h5" align="left">
+              {post.outfitName}
+            </Typography>
+
+            <Typography
+              style={{ paddingLeft: "10px", paddingTop: "10px" }}
+              variant="subtitle2"
+              color="text.secondary"
+              component="div"
+              align="right"
+            >
+              by {post.username}
+            </Typography>
+            {post.username == localStorage.getItem("userName") && (
+              <IconButton
+                aria-label="edit"
+                size="small"
+                color="primary"
+              >
+                <EditIcon />
+              </IconButton>
+            )}
+          </Box>
+          <Typography
+            variant="subtitle1"
+            color="text.secondary"
+            component="div"
+            align="left"
+          >
+            {post.description}
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            color="text.secondary"
+            component="div"
+            align="left"
+            onClick={() => {
+              routeOutfitPage(post._id);
+            }}
+          >
+            {post.comments?.length} Comments
+          </Typography>
+        </CardContent>
+      </Box>
+    </Card>
+  );
+};
+
+export default OutfitCard;
