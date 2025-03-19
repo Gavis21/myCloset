95c4e9f97db1469ba48feb3eb2c64517753e4a82
Liron Shriki <liron757@gmail.com>
2025-03-19 22:15:23 +0200

    Integrated edit page to router

--- front/src/layout/PublicLayout.tsx
+++ front/src/layout/PublicLayout.tsx
@@ -5,6 +5,7 @@ import OutfitPage from "../pages/outfit-page/OutfitPage.tsx";
 import SignIn from "../pages/sign-in/SignIn.tsx";
 import AuthContext from "../auth/AuthContext.tsx";
 import SignUp from "../pages/sign-up/SignUp.tsx";
+import EditProfile from "../pages/edit-profile-page/EditProfile.tsx";
 
 function useQuery() {
   const { search } = useLocation();
@@ -47,6 +48,17 @@ export default function PublicLayout() {
                 isAuthenticated={Object.keys(user).length > 0}
               >
                 <OutfitPage postId={query.get("postId")} />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/editProfile"
+            element={
+              <RequireAuth
+                redirectTo="/signIn"
+                isAuthenticated={Object.keys(user).length > 0}
+              >
+                <EditProfile />
               </RequireAuth>
             }
           />
